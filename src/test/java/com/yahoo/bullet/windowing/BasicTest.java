9da76569daf69dcf696383872c16f697245c8b10
Akshai Sarma <asarma@yahoo-inc.com>
2018-01-23 19:00:23 -0800

    Basic tests

--- src/test/java/com/yahoo/bullet/windowing/BasicTest.java
+++ src/test/java/com/yahoo/bullet/windowing/BasicTest.java
@@ -9,6 +9,7 @@ import com.yahoo.bullet.aggregations.MockStrategy;
 import com.yahoo.bullet.common.BulletConfig;
 import com.yahoo.bullet.result.Clip;
 import com.yahoo.bullet.result.Meta;
+import lombok.Setter;
 import org.apache.commons.lang3.tuple.Pair;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -21,6 +22,17 @@ import static com.yahoo.bullet.TestHelpers.addMetadata;
 import static java.util.Arrays.asList;
 
 public class BasicTest {
+    private class ClosableStrategy extends MockStrategy {
+        @Setter
+        private boolean closed = false;
+
+        @Override
+        public boolean isClosed() {
+            super.isClosed();
+            return closed;
+        }
+    }
+
     private static List<Map.Entry<Meta.Concept, String>> ALL_METADATA =
         asList(Pair.of(Meta.Concept.WINDOW_METADATA, "window_stats"),
                Pair.of(Meta.Concept.WINDOW_NAME, "name"),
@@ -32,6 +44,7 @@ public class BasicTest {
         MockStrategy strategy = new MockStrategy();
         Basic basic = new Basic(strategy, null, config);
         Assert.assertNotNull(basic.getMetadata());
+        Assert.assertFalse(basic.initialize().isPresent());
     }
 
     @Test
@@ -79,6 +92,26 @@ public class BasicTest {
     }
 
     @Test
+    public void testEverythingClosedOnlyIfStrategyIsClosed() {
+        BulletConfig config = new BulletConfig().validate();
+        ClosableStrategy strategy = new ClosableStrategy();
+        Basic basic = new Basic(strategy, null, config);
+        Assert.assertEquals(strategy.getClosedCalls(), 0);
+
+        Assert.assertFalse(basic.isClosed());
+        Assert.assertEquals(strategy.getClosedCalls(), 1);
+        Assert.assertFalse(basic.isClosedForPartition());
+        Assert.assertEquals(strategy.getClosedCalls(), 2);
+
+        strategy.setClosed(true);
+
+        Assert.assertTrue(basic.isClosed());
+        Assert.assertEquals(strategy.getClosedCalls(), 3);
+        Assert.assertTrue(basic.isClosedForPartition());
+        Assert.assertEquals(strategy.getClosedCalls(), 4);
+    }
+
+    @Test
     public void testDisablingMetadataDoesNotGetStrategyMetadata() {
         BulletConfig config = new BulletConfig();
         addMetadata(config, (List<Map.Entry<Meta.Concept, String>>) null);
@@ -115,6 +148,5 @@ public class BasicTest {
         Assert.assertEquals(asMap.get("num"), 4L);
         Assert.assertEquals(strategy.getMetadataCalls(), 2);
         Assert.assertEquals(strategy.getResetCalls(), 3);
-
     }
 }
