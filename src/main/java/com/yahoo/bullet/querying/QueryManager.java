80a197e3fd05cc5cb89036225c5e968d3bd5e30a
Akshai Sarma <asarma@yahoo-inc.com>
2018-11-05 14:37:44 -0800

    Removing unused file for now

--- src/main/java/com/yahoo/bullet/querying/QueryManager.java
+++
@@ -1,9 +0,0 @@
-/*
- *  Copyright 2018, Yahoo Inc.
- *  Licensed under the terms of the Apache License, Version 2.0.
- *  See the LICENSE file associated with the project for terms.
- */
-package com.yahoo.bullet.querying;
-
-public class QueryManager {
-}
