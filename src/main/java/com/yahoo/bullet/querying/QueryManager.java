89b20784120672173bb2a332a523e889df1ed05d
Akshai Sarma <asarma@yahoo-inc.com>
2018-11-05 14:36:52 -0800

    Adding a query manager and categorizer from bullet storm

--- src/main/java/com/yahoo/bullet/querying/QueryManager.java
+++ src/main/java/com/yahoo/bullet/querying/QueryManager.java
@@ -5,5 +5,82 @@
  */
 package com.yahoo.bullet.querying;
 
+import com.yahoo.bullet.common.BulletConfig;
+import com.yahoo.bullet.parsing.Query;
+import com.yahoo.bullet.querying.partitioning.Partitioner;
+import com.yahoo.bullet.record.BulletRecord;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+@Slf4j
 public class QueryManager {
+    private Map<String, Set<String>> partitioning;
+    private Map<String, Querier> queries;
+    private Partitioner partitioner;
+
+    private static class NoPartitioner implements Partitioner {
+        public static final List<String> EMPTY_KEYS = Collections.singletonList("");
+        @Override
+        public List<String> getKeys(Query query) {
+            return EMPTY_KEYS;
+        }
+
+        @Override
+        public List<String> getKeys(BulletRecord record) {
+            return EMPTY_KEYS;
+        }
+    }
+
+    public QueryManager(BulletConfig config) {
+        boolean enable = config.getAs(BulletConfig.QUERY_PARTITIONER_ENABLE, Boolean.class);
+        if (enable) {
+            partitioner = config.loadConfiguredClass(BulletConfig.QUERY_PARTITIONER_CLASS_NAME);
+            log.info("Partitioning for queries is enabled. Using %s", partitioner.getClass().getName());
+        } else {
+            partitioner = new NoPartitioner();
+        }
+        partitioning = new HashMap<>();
+        queries = new HashMap<>();
+    }
+
+    public void addQuery(String id, Querier querier) {
+        Query query = querier.getRunningQuery().getQuery();
+        List<String> keys = partitioner.getKeys(query);
+        for (String key : keys) {
+            Set<String> partition = partitioning.getOrDefault(key, new HashSet<>());
+            partition.add(id);
+            partitioning.put(key, partition);
+        }
+        queries.put(id, querier);
+    }
+
+    public Querier removeQuery(String id) {
+        Querier querier = queries.get(id);
+        if (querier != null) {
+            Query query = querier.getRunningQuery().getQuery();
+            List<String> keys = partitioner.getKeys(query);
+            for (String key : keys) {
+                partitioning.get(key).remove(id);
+            }
+        }
+        return querier;
+    }
+
+    public Map<String, Querier> getQueries(BulletRecord record) {
+        List<String> keys = partitioner.getKeys(record);
+        Map<String, Querier> queriers = new HashMap<>();
+        for (String key : keys) {
+            Set<String> queryIDs = partitioning.get(key);
+            if (queryIDs != null) {
+                queryIDs.forEach(id -> queriers.put(id, queries.get(id)));
+            }
+        }
+        return queriers;
+    }
 }
