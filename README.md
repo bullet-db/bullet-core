ff859a4774a4e586468e68e9f13887f9a1d5a9a9
Akshai Sarma <asarma@yahoo-inc.com>
2018-11-15 18:16:41 -0800

    Adding a hasDone map. Adding docs

--- README.md
+++ README.md
@@ -2,11 +2,24 @@
 
 [![Build Status](https://travis-ci.org/bullet-db/bullet-core.svg?branch=master)](https://travis-ci.org/bullet-db/bullet-core) [![Coverage Status](https://coveralls.io/repos/github/bullet-db/bullet-core/badge.svg?branch=master)](https://coveralls.io/github/bullet-db/bullet-core?branch=master) [![Download](https://api.bintray.com/packages/yahoo/maven/bullet-core/images/download.svg) ](https://bintray.com/yahoo/maven/bullet-core/_latestVersion)
 
-## Introduction
+## Background
 
-This project contains the core of Bullet, a streaming query system that uses no persistence. See [Bullet Storm](https://github.com/bullet-db/bullet-storm), which uses this to implement Bullet on the stream processing framework [Storm](https://storm.apache.org).
+Bullet is a streaming query engine that can be plugged into any singular data stream using a Stream Processing framework like Apache [Storm](https://storm.apache.org), [Spark](https://spark.apache.org) or [Flink](https://flink.apache.org). It lets you run queries on this data stream - including hard queries like Count Distincts, Top K etc.
+In Bullet, both the queries and the data flow through the system. There is absolutely no persistence layer! Queries live as long as their duration and operate on data in-memory only. So, the queries in Bullet look *forward* in time, which is pretty unique for most querying systems.
 
-This code initially lived inside the [Bullet Storm](https://github.com/bullet-db/bullet-storm) code base up to version [0.4.3](https://github.com/bullet-db/bullet-storm/releases/tag/bullet-storm-0.4.3).
+We created Bullet initially as a simple distributed grep like tool to find events in a click stream (containing high volume - 1 million events per sec -- user interaction data) at Yahoo. In particular, we use it for validating instrumentation that generates these events by interacting with the pages ourselves and finding our own events in this data stream and validate it for the proper key/value pairs. There was nothing as light-weight and cheap as Bullet to do this task.
+There are many other use-cases for Bullet and indeed, how you use it, depends on your data stream. If you put Bullet on performance metric data, your queries might mostly be finding the 99th percentile of some latency metric etc.
+
+This project is the core library for Bullet that lets us implement Bullet agnostically on any JVM based Stream Processor. See [Bullet Storm](https://github.com/bullet-db/bullet-storm), which uses this to implement Bullet on Storm and [Bullet Spark](https://github.com/bullet-db/bullet-spark), on Spark Streaming. This code initially lived inside the [Bullet Storm](https://github.com/bullet-db/bullet-storm) code base up to Bullet Storm Version [0.4.3](https://github.com/bullet-db/bullet-storm/releases/tag/bullet-storm-0.4.3).
+
+## Install
+
+Bullet Core is a library written in Java and published to [Bintray](https://bintray.com/yahoo/maven/bullet-core) and mirrored to [JCenter](http://jcenter.bintray.com/com/yahoo/bullet/bullet-core/).
+It is meant to be used to implement Bullet on different Stream Processors or to implement a Bullet [PubSub](https://bullet-db.github.io/pubsub/architecture/). To see the various versions and set up your project for your package manager (Maven, Gradle etc), [see here](https://bullet-db.github.io/releases/#bullet-core).
+
+## Usage
+
+Once you have added a dependency for Bullet Core, use our abstractions for the PubSub, Parsing, Querying, Windowing, Partitioning, and Sketching as you need to. In particular, see how we abstract running a [Bullet Query](https://github.com/bullet-db/bullet-core/blob/master/src/main/java/com/yahoo/bullet/querying/Querier.java). You can also look at our reference implementations in [Storm](https://github.com/bullet-db/bullet-storm) and [Spark](https://github.com/bullet-db/bullet-spark) to get a better idea.
 
 ## Documentation
 
@@ -15,7 +28,7 @@ All documentation is available at **[Github Pages here](https://bullet-db.github
 ## Links
 
 * [Main Documentation](https://bullet-db.github.io/) to see documentation.
-* [Bullet Core Releases](https://bullet-db.github.io/releases/#bullet-ui) to see Bullet Core releases.
+* [Bullet Core Releases](https://bullet-db.github.io/releases/#bullet-core) to see Bullet Core releases.
 
 ### Useful Links
 
@@ -28,5 +41,10 @@ All documentation is available at **[Github Pages here](https://bullet-db.github
 * [API Examples](https://bullet-db.github.io/ws/examples/) to see what kind of queries you can run on Bullet.
 * [Setup Web Service](https://bullet-db.github.io/ws/setup/) to setup the Bullet Web Service.
 * [Setup UI](https://bullet-db.github.io/ui/setup/) to setup the Bullet UI.
+
+## Contributing
+
+All contributions are welcomed! Feel free to submit PRs for bug fixes, improvements or anything else you like! Submit issues, ask questions using Github issues as normal and we will classify it accordingly. See [Contributing](Contributing.md) for a more in-depth policy. We just ask you to respect our [Code of Conduct](Code-of-Conduct.md) while you're here.
+
 
 Code licensed under the Apache 2 license. See LICENSE file for terms.
--- src/test/java/com/yahoo/bullet/querying/QueryCategorizerTest.java
+++ src/test/java/com/yahoo/bullet/querying/QueryCategorizerTest.java
@@ -20,12 +20,13 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.times;
 
 public class QueryCategorizerTest {
-    public static Querier makeQuerier(boolean isDone, boolean isRateLimited, boolean isClosed) {
+    public static Querier makeQuerier(boolean isDone, boolean isRateLimited, boolean isClosed, boolean hasData) {
         Querier querier = mock(Querier.class);
         // querier.consume returns void and does nothing. No need to mock.
         doReturn(isRateLimited).when(querier).isExceedingRateLimit();
         doReturn(isClosed).when(querier).isClosed();
         doReturn(isDone).when(querier).isDone();
+        doReturn(hasData).when(querier).hasNewData();
         return querier;
     }
 
@@ -37,84 +38,146 @@ public class QueryCategorizerTest {
 
     @Test
     public void testNoCategory() {
-        QueryCategorizer categorized = new QueryCategorizer().categorize(make(makeQuerier(false, false, false)));
+        QueryCategorizer categorized = new QueryCategorizer().categorize(make(makeQuerier(false, false, false, false)));
         Assert.assertEquals(categorized.getClosed().size(), 0);
         Assert.assertEquals(categorized.getDone().size(), 0);
         Assert.assertEquals(categorized.getRateLimited().size(), 0);
+        Assert.assertEquals(categorized.getHasData().size(), 0);
     }
 
     @Test
     public void testDoning() {
         Map<String, Querier> queries;
-        queries = make(makeQuerier(true, true, true), makeQuerier(true, true, false),
-                       makeQuerier(true, false, false), makeQuerier(true, false, true));
+        queries = make(makeQuerier(true, true, true, true), makeQuerier(true, true, true, false),
+                       makeQuerier(true, true, false, true), makeQuerier(true, true, false, false),
+                       makeQuerier(true, false, false, false), makeQuerier(true, false, false, false),
+                       makeQuerier(true, false, true, true), makeQuerier(true, false, true, false));
 
         QueryCategorizer categorized = new QueryCategorizer().categorize(queries);
         Map<String, Querier> done = categorized.getDone();
         Map<String, Querier> rateLimited = categorized.getRateLimited();
         Map<String, Querier> closed = categorized.getClosed();
+        Map<String, Querier> hasData = categorized.getHasData();
 
-        Assert.assertEquals(done.size(), 4);
+        Assert.assertEquals(done.size(), 8);
         Assert.assertTrue(done.containsKey("0"));
         Assert.assertTrue(done.containsKey("1"));
         Assert.assertTrue(done.containsKey("2"));
         Assert.assertTrue(done.containsKey("3"));
+        Assert.assertTrue(done.containsKey("4"));
+        Assert.assertTrue(done.containsKey("5"));
+        Assert.assertTrue(done.containsKey("6"));
+        Assert.assertTrue(done.containsKey("7"));
 
         Assert.assertEquals(rateLimited.size(), 0);
 
         Assert.assertEquals(closed.size(), 0);
+
+        Assert.assertEquals(hasData.size(), 0);
     }
 
     @Test
     public void testRateLimiting() {
         Map<String, Querier> queries;
-        queries = make(makeQuerier(false, true, false), makeQuerier(true, true, true),
-                       makeQuerier(true, true, false), makeQuerier(false, true, true));
+        queries = make(makeQuerier(false, true, false, true), makeQuerier(false, true, false, false),
+                       makeQuerier(true, true, true, true), makeQuerier(true, true, true, false),
+                       makeQuerier(true, true, false, true), makeQuerier(true, true, false, false),
+                       makeQuerier(false, true, true, true), makeQuerier(false, true, true, false));
 
         QueryCategorizer categorized = new QueryCategorizer().categorize(queries);
         Map<String, Querier> done = categorized.getDone();
         Map<String, Querier> rateLimited = categorized.getRateLimited();
         Map<String, Querier> closed = categorized.getClosed();
+        Map<String, Querier> hasData = categorized.getHasData();
 
-        Assert.assertEquals(done.size(), 2);
-        Assert.assertTrue(done.containsKey("1"));
+        Assert.assertEquals(done.size(), 4);
         Assert.assertTrue(done.containsKey("2"));
+        Assert.assertTrue(done.containsKey("3"));
+        Assert.assertTrue(done.containsKey("4"));
+        Assert.assertTrue(done.containsKey("5"));
 
-        Assert.assertEquals(rateLimited.size(), 2);
+        Assert.assertEquals(rateLimited.size(), 4);
         Assert.assertTrue(rateLimited.containsKey("0"));
-        Assert.assertTrue(rateLimited.containsKey("3"));
+        Assert.assertTrue(rateLimited.containsKey("1"));
+        Assert.assertTrue(rateLimited.containsKey("6"));
+        Assert.assertTrue(rateLimited.containsKey("7"));
 
         Assert.assertEquals(closed.size(), 0);
+
+        Assert.assertEquals(hasData.size(), 0);
     }
 
     @Test
     public void testClosing() {
         Map<String, Querier> queries;
-        queries = make(makeQuerier(false, true, true), makeQuerier(true, true, true),
-                       makeQuerier(true, false, true), makeQuerier(false, false, true));
+        queries = make(makeQuerier(false, true, true, true), makeQuerier(false, true, true, false),
+                       makeQuerier(true, true, true, true), makeQuerier(true, true, true, false),
+                       makeQuerier(true, false, true, true), makeQuerier(true, false, true, false),
+                       makeQuerier(false, false, true, true), makeQuerier(false, false, true, false));
 
         QueryCategorizer categorized = new QueryCategorizer().categorize(queries);
         Map<String, Querier> done = categorized.getDone();
         Map<String, Querier> rateLimited = categorized.getRateLimited();
         Map<String, Querier> closed = categorized.getClosed();
+        Map<String, Querier> hasData = categorized.getHasData();
 
-        Assert.assertEquals(done.size(), 2);
-        Assert.assertTrue(done.containsKey("1"));
+        Assert.assertEquals(done.size(), 4);
         Assert.assertTrue(done.containsKey("2"));
+        Assert.assertTrue(done.containsKey("3"));
+        Assert.assertTrue(done.containsKey("4"));
+        Assert.assertTrue(done.containsKey("5"));
 
-        Assert.assertEquals(rateLimited.size(), 1);
+        Assert.assertEquals(rateLimited.size(), 2);
         Assert.assertTrue(rateLimited.containsKey("0"));
+        Assert.assertTrue(rateLimited.containsKey("1"));
+
+        Assert.assertEquals(closed.size(), 2);
+        Assert.assertTrue(closed.containsKey("6"));
+        Assert.assertTrue(closed.containsKey("7"));
+
+        Assert.assertEquals(hasData.size(), 0);
+    }
+
+    @Test
+    public void testHavingData() {
+        Map<String, Querier> queries;
+        queries = make(makeQuerier(true, true, true, true), makeQuerier(false, true, true, true),
+                       makeQuerier(false, false, true, true), makeQuerier(false, false, false, true),
+                       makeQuerier(false, true, true, true), makeQuerier(true, true, false, true),
+                       makeQuerier(false, true, false, true), makeQuerier(true, false, false, true));
+
+        QueryCategorizer categorized = new QueryCategorizer().categorize(queries);
+        Map<String, Querier> done = categorized.getDone();
+        Map<String, Querier> rateLimited = categorized.getRateLimited();
+        Map<String, Querier> closed = categorized.getClosed();
+        Map<String, Querier> hasData = categorized.getHasData();
+
+        Assert.assertEquals(done.size(), 3);
+        Assert.assertTrue(done.containsKey("0"));
+        Assert.assertTrue(done.containsKey("5"));
+        Assert.assertTrue(done.containsKey("7"));
+
+        Assert.assertEquals(rateLimited.size(), 3);
+        Assert.assertTrue(rateLimited.containsKey("1"));
+        Assert.assertTrue(rateLimited.containsKey("4"));
+        Assert.assertTrue(rateLimited.containsKey("6"));
 
         Assert.assertEquals(closed.size(), 1);
-        Assert.assertTrue(closed.containsKey("3"));
+        Assert.assertTrue(closed.containsKey("2"));
+
+        Assert.assertEquals(hasData.size(), 1);
+        Assert.assertTrue(hasData.containsKey("3"));
     }
 
 
     @Test
     public void testConsuming() {
         Map<String, Querier> queries;
-        queries = make(makeQuerier(true, false, false), makeQuerier(false, true, false),
-                       makeQuerier(false, false, true), makeQuerier(false, false, false));
+        queries = make(makeQuerier(true, false, false, false),
+                       makeQuerier(false, true, false, false),
+                       makeQuerier(false, false, true, false),
+                       makeQuerier(false, false, false, true),
+                       makeQuerier(false, false, false, false));
 
         BulletRecord record = RecordBox.get().getRecord();
 
@@ -122,6 +185,7 @@ public class QueryCategorizerTest {
         Map<String, Querier> done = categorized.getDone();
         Map<String, Querier> rateLimited = categorized.getRateLimited();
         Map<String, Querier> closed = categorized.getClosed();
+        Map<String, Querier> hasData = categorized.getHasData();
 
         Assert.assertEquals(done.size(), 1);
         Assert.assertTrue(done.containsKey("0"));
@@ -131,6 +195,9 @@ public class QueryCategorizerTest {
 
         Assert.assertEquals(closed.size(), 1);
         Assert.assertTrue(closed.containsKey("2"));
+
+        Assert.assertEquals(hasData.size(), 1);
+        Assert.assertTrue(hasData.containsKey("3"));
 
         // Test record was consumed by all the queries, including the one that was not categorized
         for (Querier querier : queries.values()) {
