376f3cfb36b2d082532fb169b7e5b4007f8b8448
Brian Xiao <bxiao@oath.com>
2020-05-31 16:47:29 -0700

    projection and filter tests

--- src/test/java/com/yahoo/bullet/querying/ProjectionTest.java
+++ src/test/java/com/yahoo/bullet/querying/ProjectionTest.java
@@ -5,5 +5,73 @@
  */
 package com.yahoo.bullet.querying;
 
+import com.yahoo.bullet.query.Field;
+import com.yahoo.bullet.query.expressions.BinaryExpression;
+import com.yahoo.bullet.query.expressions.FieldExpression;
+import com.yahoo.bullet.query.expressions.Operation;
+import com.yahoo.bullet.record.BulletRecord;
+import com.yahoo.bullet.record.avro.TypedAvroBulletRecordProvider;
+import com.yahoo.bullet.result.RecordBox;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class ProjectionTest {
+    @Test
+    public void testProjectNewRecord() {
+        BulletRecord record = RecordBox.get().add("a", 2)
+                                             .add("b", 4)
+                                             .add("c", 6).getRecord();
+
+        List<Field> fields = Arrays.asList(new Field("d", new BinaryExpression(new FieldExpression("a"),
+                                                                               new FieldExpression("b"),
+                                                                               Operation.ADD)),
+                                           new Field("e", new BinaryExpression(new FieldExpression("b"),
+                                                                               new FieldExpression("c"),
+                                                                               Operation.MUL)),
+                                           new Field("f", new BinaryExpression(new FieldExpression("d"),
+                                                                               new FieldExpression("e"),
+                                                                               Operation.ADD)),
+                                           new Field("g", new FieldExpression("g")));
+
+        Projection projection = new Projection(fields);
+        BulletRecord newRecord = projection.project(record, new TypedAvroBulletRecordProvider());
+
+        Assert.assertEquals(newRecord.fieldCount(), 2);
+        Assert.assertEquals(newRecord.typedGet("d").getValue(), 6);
+        Assert.assertEquals(newRecord.typedGet("e").getValue(), 24);
+    }
+
+    @Test
+    public void testProjectOldRecord() {
+        BulletRecord record = RecordBox.get().add("a", 2)
+                                             .add("b", 4)
+                                             .add("c", 6).getRecord();
+
+        List<Field> fields = Arrays.asList(new Field("a", new BinaryExpression(new FieldExpression("a"),
+                                                                               new FieldExpression("b"),
+                                                                               Operation.MUL)),
+                                           new Field("b", new BinaryExpression(new FieldExpression("b"),
+                                                                               new FieldExpression("c"),
+                                                                               Operation.MUL)),
+                                           new Field("d", new BinaryExpression(new FieldExpression("b"),
+                                                                               new FieldExpression("c"),
+                                                                               Operation.ADD)),
+                                           new Field("e", new BinaryExpression(new FieldExpression("e"),
+                                                                               new FieldExpression("f"),
+                                                                               Operation.SUB)),
+                                           new Field("f", new FieldExpression("f")));
+
+        Projection projection = new Projection(fields);
+        BulletRecord oldRecord = projection.project(record);
+
+        Assert.assertEquals(oldRecord.fieldCount(), 4);
+        Assert.assertEquals(oldRecord.typedGet("a").getValue(), 8);
+        Assert.assertEquals(oldRecord.typedGet("b").getValue(), 24);
+        Assert.assertEquals(oldRecord.typedGet("c").getValue(), 6);
+        Assert.assertEquals(oldRecord.typedGet("d").getValue(), 10);
+        Assert.assertEquals(oldRecord, record);
+    }
 }
