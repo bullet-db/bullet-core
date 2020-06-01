241324d6f5904d55f1c9d4117c77ccebf4dcc5bf
Brian Xiao <bxiao@oath.com>
2020-05-31 21:46:02 -0700

    finished simpleequalitypartitioner tests

--- src/test/java/com/yahoo/bullet/querying/partitioning/SimpleEqualityPartitionerTest.java
+++ src/test/java/com/yahoo/bullet/querying/partitioning/SimpleEqualityPartitionerTest.java
@@ -10,6 +10,13 @@ import com.yahoo.bullet.query.Projection;
 import com.yahoo.bullet.query.Window;
 import com.yahoo.bullet.query.Query;
 import com.yahoo.bullet.query.aggregations.Raw;
+import com.yahoo.bullet.query.expressions.BinaryExpression;
+import com.yahoo.bullet.query.expressions.Expression;
+import com.yahoo.bullet.query.expressions.FieldExpression;
+import com.yahoo.bullet.query.expressions.ListExpression;
+import com.yahoo.bullet.query.expressions.Operation;
+import com.yahoo.bullet.query.expressions.UnaryExpression;
+import com.yahoo.bullet.query.expressions.ValueExpression;
 import com.yahoo.bullet.record.BulletRecord;
 import com.yahoo.bullet.result.RecordBox;
 import org.apache.commons.lang3.tuple.ImmutablePair;
@@ -21,6 +28,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 import static java.util.Collections.singleton;
 
 public class SimpleEqualityPartitionerTest {
@@ -46,6 +54,20 @@ public class SimpleEqualityPartitionerTest {
         return query;
     }
 
+    private Query createQuery(Expression filter) {
+        Query query = new Query(new Projection(), filter, new Raw(null), null, new Window(), null);
+        query.configure(config);
+        return query;
+    }
+
+    private Query createQuery(Expression... filters) {
+        Expression filter = filters[0];
+        for (int i = 1; i < filters.length; i++) {
+            filter = new BinaryExpression(filter, filters[i], Operation.AND);
+        }
+        return createQuery(filter);
+    }
+
     @Test
     public void testDefaultPartitioningQueryWithNoFilters() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
@@ -53,90 +75,84 @@ public class SimpleEqualityPartitionerTest {
     }
 
     @Test
-    public void testDefaultPartitioningQueryWithNoLogicalFilters() {
+    public void testDefaultPartitioningQueryWithNoEqualityFilters() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
-        //Assert.assertEquals(partitioner.getKeys(createQuery(makeClause(Clause.Operation.AND))), singleton("null-null"));
+        Query query = createQuery(new FieldExpression("abc"));
+        Assert.assertEquals(partitioner.getKeys(query), singleton("*-*"));
     }
-/*
+
+    @Test
+    public void testDefaultPartitioningQueryWithNullEqualityFilters() {
+        SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
+        Query query = createQuery(new BinaryExpression(new FieldExpression("A"), new ValueExpression(null), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("B"), new ValueExpression(null), Operation.EQUALS));
+        Assert.assertEquals(partitioner.getKeys(query), singleton("null-null"));
+    }
+
     @Test
     public void testDefaultPartitioningQueryWithUnrelatedFilters() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
-        Query query = createQuery(makeClause("C", singletonList("bar"), Clause.Operation.EQUALS),
-                                  makeClause("D", singletonList("baz"), Clause.Operation.EQUALS));
-        Assert.assertEquals(partitioner.getKeys(query), singleton("null-null"));
+        Query query = createQuery(new BinaryExpression(new FieldExpression("C"), new ValueExpression("bar"), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("D"), new ValueExpression("baz"), Operation.EQUALS));
+        Assert.assertEquals(partitioner.getKeys(query), singleton("*-*"));
     }
 
     @Test
     public void testDefaultPartitioningQueryWithNonEqualityFilters() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
-        Query query = createQuery(makeClause("A", singletonList("bar"), Clause.Operation.REGEX_LIKE),
-                                  makeClause("B", singletonList("baz"), Clause.Operation.CONTAINS_KEY));
-        Assert.assertEquals(partitioner.getKeys(query), singleton("null-null"));
+        Query query = createQuery(new BinaryExpression(new FieldExpression("A"), new ValueExpression("bar"), Operation.NOT_EQUALS),
+                                  new BinaryExpression(new FieldExpression("B"), new ValueExpression("baz"), Operation.NOT_EQUALS));
+        Assert.assertEquals(partitioner.getKeys(query), singleton("*-*"));
     }
 
     @Test
     public void testDefaultPartitioningQueryWithOR() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
-        Query query = createQuery(makeClause(Clause.Operation.OR,
-                                             makeClause("A", singletonList("bar"), Clause.Operation.EQUALS),
-                                             makeClause("B", singletonList("baz"), Clause.Operation.EQUALS)));
-        Assert.assertEquals(partitioner.getKeys(query), singleton("null-null"));
+        Query query = createQuery(new BinaryExpression(new BinaryExpression(new FieldExpression("A"), new ValueExpression("bar"), Operation.EQUALS),
+                                                       new BinaryExpression(new FieldExpression("B"), new ValueExpression("baz"), Operation.EQUALS),
+                                                       Operation.OR));
+        Assert.assertEquals(partitioner.getKeys(query), singleton("*-*"));
     }
 
     @Test
     public void testDefaultPartitioningQueryWithNOT() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
-        Query query = createQuery(makeClause(Clause.Operation.NOT, makeClause("A", singletonList("bar"), Clause.Operation.EQUALS)));
-        Assert.assertEquals(partitioner.getKeys(query), singleton("null-null"));
-    }
-
-    @Test
-    public void testDefaultPartitioningForQueryWithMissingValues() {
-        SimpleEqualityPartitioner partitioner = createPartitioner("A");
-        FilterClause clause = new StringFilterClause();
-        clause.setField("A");
-        clause.setValues(null);
-        clause.setOperation(Clause.Operation.EQUALS);
-        Query query = createQuery(clause);
-        Assert.assertEquals(partitioner.getKeys(query), singleton("null"));
+        Query query = createQuery(new UnaryExpression(new BinaryExpression(new FieldExpression("A"), new ValueExpression("bar"), Operation.EQUALS),
+                                                      Operation.NOT));
+        Assert.assertEquals(partitioner.getKeys(query), singleton("*-*"));
     }
 
     @Test
     public void testDefaultPartitioningForQueryWithMultipleValues() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
-        Query query = createQuery(makeClause(Clause.Operation.AND,
-                                             makeClause("A", asList("foo", "bar"), Clause.Operation.EQUALS),
-                                             makeClause("B", singletonList("baz"), Clause.Operation.EQUALS)));
-        Assert.assertEquals(partitioner.getKeys(query), singleton("null-null"));
+        Query query = createQuery(new BinaryExpression(new FieldExpression("A"), new ValueExpression("foo"), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("A"), new ValueExpression("bar"), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("B"), new ValueExpression("baz"), Operation.EQUALS));
+        Assert.assertEquals(partitioner.getKeys(query), singleton("*-*"));
     }
 
     @Test
-    public void testDefaultPartitioningForQueryWithRepeatedFilters() {
-        SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
-        Query query = createQuery(makeClause(Clause.Operation.AND,
-                                             makeClause(Clause.Operation.AND,
-                                                        makeClause("A", singletonList("quux"), Clause.Operation.EQUALS),
-                                                        makeClause("B", singletonList("norf"), Clause.Operation.EQUALS)),
-                                             makeClause(Clause.Operation.AND,
-                                                        makeClause("B", singletonList("qux"), Clause.Operation.EQUALS),
-                                                        makeClause("A", singletonList("bar"), Clause.Operation.EQUALS))));
-        Assert.assertEquals(partitioner.getKeys(query), singletonList("null-null"));
+    public void testDefaultPartitioningForQueryWthImproperBinaryEquals() {
+        SimpleEqualityPartitioner partitioner = createPartitioner("A");
+        Query query = createQuery(new BinaryExpression(new FieldExpression("A"), new FieldExpression("B"), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("A"), new FieldExpression("B"), Operation.EQUALS),
+                                  new BinaryExpression(new ValueExpression("foo"), new ValueExpression("bar"), Operation.EQUALS),
+                                  new BinaryExpression(new ListExpression(emptyList()), new ListExpression(emptyList()), Operation.EQUALS));
+        Assert.assertEquals(partitioner.getKeys(query), singleton("*"));
     }
 
     @Test
     public void testPartitioningForQueryWithMissingFields() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
-        Query query = createQuery(makeClause(Clause.Operation.AND, makeClause("A", singletonList("bar"), Clause.Operation.EQUALS)));
-        Assert.assertEquals(partitioner.getKeys(query), singleton("bar.-null"));
+        Query query = createQuery(new BinaryExpression(new FieldExpression("A"), new ValueExpression("bar"), Operation.EQUALS));
+        Assert.assertEquals(partitioner.getKeys(query), singleton("bar.-*"));
     }
 
     @Test
     public void testPartitioningForQueryWithNullCheckedFields() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
-        // Has an ObjectFilterClause with type forced to STRING (so a == "null" as opposed to is not null)
-        Query query = createQuery(makeClause(Clause.Operation.AND,
-                                             makeObjectClause("A", singletonList(new Value(Value.Kind.VALUE, DistributionType.NULL_EXPRESSION, DistributionType.STRING)), Clause.Operation.EQUALS),
-                                             makeStringClause("B", null, Clause.Operation.EQUALS)));
+        Query query = createQuery(new BinaryExpression(new FieldExpression("A"), new ValueExpression("null"), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("B"), new ValueExpression(null), Operation.EQUALS));
 
         Assert.assertEquals(partitioner.getKeys(query), singleton("null.-null"));
     }
@@ -144,26 +160,31 @@ public class SimpleEqualityPartitionerTest {
     @Test
     public void testPartitioningForQueryWithAllFields() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B", "C");
-        Query query = createQuery(makeClause(Clause.Operation.AND,
-                                             makeClause("C", singletonList("qux"), Clause.Operation.EQUALS),
-                                             makeClause("B", singletonList("baz"), Clause.Operation.EQUALS),
-                                             makeClause("A", singletonList("bar"), Clause.Operation.EQUALS)));
+        Query query = createQuery(new BinaryExpression(new FieldExpression("A"), new ValueExpression("bar"), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("B"), new ValueExpression("baz"), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("C"), new ValueExpression("qux"), Operation.EQUALS));
+        Assert.assertEquals(partitioner.getKeys(query), singleton("bar.-baz.-qux."));
+    }
+
+    @Test
+    public void testPartitioningForQueryWithAllFieldsOperandsFlipped() {
+        SimpleEqualityPartitioner partitioner = createPartitioner("A", "B", "C");
+        Query query = createQuery(new BinaryExpression(new ValueExpression("bar"),new FieldExpression("A"), Operation.EQUALS),
+                                  new BinaryExpression(new ValueExpression("baz"), new FieldExpression("B"), Operation.EQUALS),
+                                  new BinaryExpression(new ValueExpression("qux"),new FieldExpression("C"), Operation.EQUALS));
         Assert.assertEquals(partitioner.getKeys(query), singleton("bar.-baz.-qux."));
     }
 
     @Test
     public void testPartitioningForQueryWithNestedFields() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B", "C", "D.e");
-        Query query = createQuery(makeClause(Clause.Operation.AND,
-                                             makeClause(Clause.Operation.AND,
-                                                        makeClause("B", singletonList("quux"), Clause.Operation.EQUALS),
-                                                        makeClause("D.e", singletonList("norf"), Clause.Operation.EQUALS)),
-                                             makeClause(Clause.Operation.AND,
-                                                        makeClause("C", singletonList("qux"), Clause.Operation.EQUALS),
-                                                        makeClause("A", singletonList("bar"), Clause.Operation.EQUALS))));
+        Query query = createQuery(new BinaryExpression(new FieldExpression("A"), new ValueExpression("bar"), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("B"), new ValueExpression("quux"), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("C"), new ValueExpression("qux"), Operation.EQUALS),
+                                  new BinaryExpression(new FieldExpression("D", "e"), new ValueExpression("norf"), Operation.EQUALS));
         Assert.assertEquals(partitioner.getKeys(query), singleton("bar.-quux.-qux.-norf."));
     }
-*/
+
     @Test
     public void testPartitioningForRecordWithMissingFields() {
         SimpleEqualityPartitioner partitioner = createPartitioner("A", "B");
