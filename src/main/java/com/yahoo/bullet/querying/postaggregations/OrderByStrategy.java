fb046c339067d14cb2997e04000ff8a49ba9a7bb
0aix <bxiao@oath.com>
2020-08-10 13:55:59 -0700

    OrderBy using expressions (#83)

--- src/main/java/com/yahoo/bullet/querying/postaggregations/OrderByStrategy.java
+++ src/main/java/com/yahoo/bullet/querying/postaggregations/OrderByStrategy.java
@@ -5,18 +5,52 @@
  */
 package com.yahoo.bullet.querying.postaggregations;
 
+import com.yahoo.bullet.query.expressions.Expression;
 import com.yahoo.bullet.query.postaggregations.OrderBy;
+import com.yahoo.bullet.querying.evaluators.Evaluator;
 import com.yahoo.bullet.record.BulletRecord;
 import com.yahoo.bullet.result.Clip;
 import com.yahoo.bullet.typesystem.TypedObject;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class OrderByStrategy implements PostStrategy {
+    private static final Comparator<TypedObject> NULLS_FIRST = TypedObject.nullsFirst();
+
     private final Comparator<BulletRecord> comparator;
+    private final List<Evaluator> evaluators;
+    private final List<OrderBy.Direction> directions;
+    private final Map<BulletRecord, LazyArray> mapping;
+    private final int numberOfFields;
+
+    private class LazyArray {
+        private final BulletRecord record;
+        private final TypedObject[] values;
+
+        private LazyArray(BulletRecord record, int capacity) {
+            this.record = record;
+            this.values = new TypedObject[capacity];
+        }
+
+        private TypedObject get(int index) {
+            TypedObject value = values[index];
+            if (value == null) {
+                try {
+                    value = evaluators.get(index).evaluate(record);
+                } catch (Exception e) {
+                    value = TypedObject.NULL;
+                }
+                values[index] = value;
+            }
+            return value;
+        }
+    }
 
     /**
      * Constructor that creates an OrderBy post-strategy.
@@ -24,26 +58,35 @@ public class OrderByStrategy implements PostStrategy {
      * @param orderBy The OrderBy post-aggregation to create a strategy for.
      */
     public OrderByStrategy(OrderBy orderBy) {
-        comparator = getComparator(orderBy);
+        evaluators = orderBy.getFields().stream().map(OrderBy.SortItem::getExpression).map(Expression::getEvaluator).collect(Collectors.toList());
+        directions = orderBy.getFields().stream().map(OrderBy.SortItem::getDirection).collect(Collectors.toList());
+        mapping = new HashMap<>();
+        numberOfFields = orderBy.getFields().size();
+        comparator = getComparator();
     }
 
     @Override
     public Clip execute(Clip clip) {
         List<BulletRecord> records = clip.getRecords();
+        records.forEach(record -> mapping.put(record, new LazyArray(record, numberOfFields)));
         records.sort(comparator);
+        mapping.clear();
         return clip;
     }
 
-    private static Comparator<BulletRecord> getComparator(OrderBy orderBy) {
-        return orderBy.getFields().stream().map(OrderByStrategy::fieldComparator).reduce(Comparator::thenComparing).get();
-    }
-
-    private static Comparator<BulletRecord> fieldComparator(OrderBy.SortItem sortItem) {
+    private Comparator<BulletRecord> getComparator() {
         return (a, b) -> {
-            TypedObject typedObjectA = a.typedGet(sortItem.getField());
-            TypedObject typedObjectB = b.typedGet(sortItem.getField());
-            return sortItem.getDirection() == OrderBy.Direction.ASC ? TypedObject.nullsFirst().compare(typedObjectA, typedObjectB)
-                                                                    : TypedObject.nullsFirst().compare(typedObjectB, typedObjectA);
+            LazyArray lazyArrayA = mapping.get(a);
+            LazyArray lazyArrayB = mapping.get(b);
+            int c;
+            for (int i = 0; i < numberOfFields; i++) {
+                c = directions.get(i) == OrderBy.Direction.ASC ? NULLS_FIRST.compare(lazyArrayA.get(i), lazyArrayB.get(i))
+                                                               : NULLS_FIRST.compare(lazyArrayB.get(i), lazyArrayA.get(i));
+                if (c != 0) {
+                    return c;
+                }
+            }
+            return 0;
         };
     }
 }
--- src/test/java/com/yahoo/bullet/querying/postaggregations/OrderByStrategyTest.java
+++ src/test/java/com/yahoo/bullet/querying/postaggregations/OrderByStrategyTest.java
@@ -5,6 +5,11 @@
  */
 package com.yahoo.bullet.querying.postaggregations;
 
+import com.yahoo.bullet.query.expressions.BinaryExpression;
+import com.yahoo.bullet.query.expressions.FieldExpression;
+import com.yahoo.bullet.query.expressions.Operation;
+import com.yahoo.bullet.query.expressions.UnaryExpression;
+import com.yahoo.bullet.query.expressions.ValueExpression;
 import com.yahoo.bullet.query.postaggregations.OrderBy;
 import com.yahoo.bullet.record.BulletRecord;
 import com.yahoo.bullet.result.Clip;
@@ -14,6 +19,7 @@ import org.testng.annotations.Test;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class OrderByStrategyTest {
@@ -23,7 +29,8 @@ public class OrderByStrategyTest {
 
     @Test
     public void testOrderByAscending() {
-        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem("a", OrderBy.Direction.ASC), new OrderBy.SortItem("b", OrderBy.Direction.ASC)));
+        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem(new FieldExpression("a"), OrderBy.Direction.ASC),
+                                                                    new OrderBy.SortItem(new FieldExpression("b"), OrderBy.Direction.ASC)));
         List<BulletRecord> records = new ArrayList<>();
         records.add(RecordBox.get().add("a", 5).add("b", 2).getRecord());
         records.add(RecordBox.get().add("a", 2).add("b", 4).getRecord());
@@ -45,7 +52,8 @@ public class OrderByStrategyTest {
 
     @Test
     public void testOrderByDescending() {
-        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem("a", OrderBy.Direction.DESC), new OrderBy.SortItem("b", OrderBy.Direction.DESC)));
+        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem(new FieldExpression("a"), OrderBy.Direction.DESC),
+                                                                    new OrderBy.SortItem(new FieldExpression("b"), OrderBy.Direction.DESC)));
         List<BulletRecord> records = new ArrayList<>();
         records.add(RecordBox.get().add("a", 5).add("b", 2).getRecord());
         records.add(RecordBox.get().add("a", 2).add("b", 4).getRecord());
@@ -67,7 +75,8 @@ public class OrderByStrategyTest {
 
     @Test
     public void testOrderByAscendingWithDifferentType() {
-        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem("a", OrderBy.Direction.ASC), new OrderBy.SortItem("b", OrderBy.Direction.ASC)));
+        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem(new FieldExpression("a"), OrderBy.Direction.ASC),
+                                                                    new OrderBy.SortItem(new FieldExpression("b"), OrderBy.Direction.ASC)));
         List<BulletRecord> records = new ArrayList<>();
         records.add(RecordBox.get().add("a", 5).add("b", 2.0).getRecord());
         records.add(RecordBox.get().add("a", 2).add("b", 4).getRecord());
@@ -89,7 +98,8 @@ public class OrderByStrategyTest {
 
     @Test
     public void testOrderByAscendingWithNonExistingField() {
-        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem("a", OrderBy.Direction.ASC), new OrderBy.SortItem("c", OrderBy.Direction.ASC)));
+        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem(new FieldExpression("a"), OrderBy.Direction.ASC),
+                                                                    new OrderBy.SortItem(new FieldExpression("c"), OrderBy.Direction.ASC)));
         List<BulletRecord> records = new ArrayList<>();
         records.add(RecordBox.get().add("a", 5).add("b", 2.0).getRecord());
         records.add(RecordBox.get().add("a", 2).add("b", 4).getRecord());
@@ -111,7 +121,8 @@ public class OrderByStrategyTest {
 
     @Test
     public void testOrderByAscendingWithSomeMissingField() {
-        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem("a", OrderBy.Direction.ASC), new OrderBy.SortItem("c", OrderBy.Direction.ASC)));
+        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem(new FieldExpression("a"), OrderBy.Direction.ASC),
+                                                                    new OrderBy.SortItem(new FieldExpression("c"), OrderBy.Direction.ASC)));
         List<BulletRecord> records = new ArrayList<>();
         records.add(RecordBox.get().add("a", 5).add("b", 2).getRecord());
         records.add(RecordBox.get().add("a", 2).add("b", 4).getRecord());
@@ -129,5 +140,70 @@ public class OrderByStrategyTest {
         Assert.assertEquals(result.getRecords().get(2).typedGet("b").getValue(), 4);
         Assert.assertEquals(result.getRecords().get(3).typedGet("a").getValue(), 5);
         Assert.assertEquals(result.getRecords().get(3).typedGet("b").getValue(), 2);
+    }
+
+    @Test
+    public void testOrderByComputationWithSingleField() {
+        OrderByStrategy orderByStrategy = makeOrderBy(Arrays.asList(new OrderBy.SortItem(new BinaryExpression(new FieldExpression("a"), new ValueExpression(-1), Operation.MUL), OrderBy.Direction.ASC),
+                                                                    new OrderBy.SortItem(new BinaryExpression(new FieldExpression("b"), new ValueExpression(-1), Operation.MUL), OrderBy.Direction.ASC)));
+        List<BulletRecord> records = new ArrayList<>();
+        records.add(RecordBox.get().add("a", 5).add("b", 2).getRecord());
+        records.add(RecordBox.get().add("a", 2).add("b", 4).getRecord());
+        records.add(RecordBox.get().add("a", 2).add("b", 2).getRecord());
+        records.add(RecordBox.get().add("a", 1).add("b", 7).getRecord());
+        Clip clip = new Clip();
+        clip.add(records);
+        Clip result = orderByStrategy.execute(clip);
+
+        Assert.assertEquals(result.getRecords().get(0).typedGet("a").getValue(), 5);
+        Assert.assertEquals(result.getRecords().get(0).typedGet("b").getValue(), 2);
+        Assert.assertEquals(result.getRecords().get(1).typedGet("a").getValue(), 2);
+        Assert.assertEquals(result.getRecords().get(1).typedGet("b").getValue(), 4);
+        Assert.assertEquals(result.getRecords().get(2).typedGet("a").getValue(), 2);
+        Assert.assertEquals(result.getRecords().get(2).typedGet("b").getValue(), 2);
+        Assert.assertEquals(result.getRecords().get(3).typedGet("a").getValue(), 1);
+        Assert.assertEquals(result.getRecords().get(3).typedGet("b").getValue(), 7);
+    }
+
+    @Test
+    public void testOrderByComputationWithMultipleFields() {
+        OrderByStrategy orderByStrategy = makeOrderBy(Collections.singletonList(new OrderBy.SortItem(new BinaryExpression(new FieldExpression("a"), new FieldExpression("b"), Operation.ADD), OrderBy.Direction.DESC)));
+        List<BulletRecord> records = new ArrayList<>();
+        records.add(RecordBox.get().add("a", 5).add("b", 2).getRecord());
+        records.add(RecordBox.get().add("a", 2).add("b", 4).getRecord());
+        records.add(RecordBox.get().add("a", 2).add("b", 2).getRecord());
+        records.add(RecordBox.get().add("a", 1).add("b", 7).getRecord());
+        Clip clip = new Clip();
+        clip.add(records);
+        Clip result = orderByStrategy.execute(clip);
+
+        Assert.assertEquals(result.getRecords().get(0).typedGet("a").getValue(), 1);
+        Assert.assertEquals(result.getRecords().get(0).typedGet("b").getValue(), 7);
+        Assert.assertEquals(result.getRecords().get(1).typedGet("a").getValue(), 5);
+        Assert.assertEquals(result.getRecords().get(1).typedGet("b").getValue(), 2);
+        Assert.assertEquals(result.getRecords().get(2).typedGet("a").getValue(), 2);
+        Assert.assertEquals(result.getRecords().get(2).typedGet("b").getValue(), 4);
+        Assert.assertEquals(result.getRecords().get(3).typedGet("a").getValue(), 2);
+        Assert.assertEquals(result.getRecords().get(3).typedGet("b").getValue(), 2);
+    }
+
+    @Test
+    public void testOrderByComputationWithMissingField() {
+        OrderByStrategy orderByStrategy = makeOrderBy(Collections.singletonList(new OrderBy.SortItem(new UnaryExpression(new FieldExpression("a"), Operation.SIZE_OF), OrderBy.Direction.ASC)));
+        List<BulletRecord> records = new ArrayList<>();
+        records.add(RecordBox.get().add("a", "hello").getRecord());
+        records.add(RecordBox.get().add("a", "").getRecord());
+        records.add(RecordBox.get().add("a", "foobar").getRecord());
+        records.add(RecordBox.get().getRecord());
+        records.add(RecordBox.get().add("a", "world").getRecord());
+        Clip clip = new Clip();
+        clip.add(records);
+        Clip result = orderByStrategy.execute(clip);
+
+        Assert.assertEquals(result.getRecords().get(0).typedGet("a").getValue(), null);
+        Assert.assertEquals(result.getRecords().get(1).typedGet("a").getValue(), "");
+        Assert.assertEquals(result.getRecords().get(2).typedGet("a").getValue(), "hello");
+        Assert.assertEquals(result.getRecords().get(3).typedGet("a").getValue(), "world");
+        Assert.assertEquals(result.getRecords().get(4).typedGet("a").getValue(), "foobar");
     }
 }
