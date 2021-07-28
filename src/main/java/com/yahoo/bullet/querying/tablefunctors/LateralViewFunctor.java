2ffe365888a78ed0b684d4f18ca40da3813c2601
Brian Xiao <bxiao@oath.com>
2021-07-28 14:12:26 -0400

    stash

--- src/main/java/com/yahoo/bullet/querying/tablefunctors/LateralViewFunctor.java
+++ src/main/java/com/yahoo/bullet/querying/tablefunctors/LateralViewFunctor.java
@@ -9,6 +9,7 @@ import com.yahoo.bullet.query.tablefunctions.LateralView;
 import com.yahoo.bullet.record.BulletRecord;
 import com.yahoo.bullet.record.BulletRecordProvider;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -18,7 +19,7 @@ import java.util.stream.Collectors;
 public class LateralViewFunctor extends TableFunctor {
     private static final long serialVersionUID = 1017033253024183470L;
 
-    final TableFunctor tableFunctor;
+    final List<TableFunctor> tableFunctors;
 
     /**
      * Constructor that creates a lateral view table functor from a {@link LateralView}.
@@ -31,6 +32,19 @@ public class LateralViewFunctor extends TableFunctor {
 
     @Override
     public List<BulletRecord> apply(BulletRecord record, BulletRecordProvider provider) {
+        if (tableFunctors.size() == 1) {
+            return apply(record, provider, tableFunctors.get(0));
+        }
+        List<BulletRecord> records = Collections.singletonList(record);
+        for (TableFunctor tableFunctor : tableFunctors) {
+            records = records.stream().flatMap(r -> apply(r, provider, tableFunctor).stream()).collect(Collectors.toList());
+        }
+        return records;
+        //List<BulletRecord> records = tableFunctor.apply(record, provider);
+        //return records.stream().map(generated -> new LateralViewBulletRecord(record, generated)).collect(Collectors.toList());
+    }
+
+    private List<BulletRecord> apply(BulletRecord record, BulletRecordProvider provider, TableFunctor tableFunctor) {
         List<BulletRecord> records = tableFunctor.apply(record, provider);
         return records.stream().map(generated -> new LateralViewBulletRecord(record, generated)).collect(Collectors.toList());
     }
